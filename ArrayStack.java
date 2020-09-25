c1b7e2d380405e0bb76999d76df81b58937ec721
2190007 <70031113+2190007@users.noreply.github.com>
2020-09-25 12:18:49 -0500

    Update ArrayStack.java
    Uploaded fixed ArrayStack.java class

--- ArrayStack.java
+++ ArrayStack.java
@@ -1,53 +1,65 @@
 /**
+ * Class for the ArrayStack
+ *
  * @author Jonathon Snellback
  */
 public class ArrayStack<E> implements Stack<E>
 {
 	Object[] stack = new Object[100];
-	    public boolean isEmpty(){
-				for(int i = 0; i < stack.length; i++){
-					if(stack[i] != null)
-					return false;
-				}
+	/**
+	* Checks if the stack is empty
+	*/
+	public boolean isEmpty(){
+		for(int i = 0; i < stack.length; i++){
+			if(stack[i] != null)
+				return false;
+		}
 				return true;
 			}
-			    public E peekTop() throws StackException{
-            if(isEmpty()){
-							throw new StackException();
-						}
-						for(int i = stack.length - 1; i>= 0; i--){
-							if(stack[i]!= null){
-								return (E)stack[i];
-							}
-						}
-						return null;
-          }
-				   public E pop() throws StackException{
-						 E value;
-
-						 if(isEmpty()){
-							 throw new StackException();
-						 }
-						 for(int i = stack.length; i >= 0; i--){
-							 if(stack[i] != null){
-								value = stack[i];
-                stack[i] = null;
-                break;
+	/**
+	* returns the top value in the stack.
+	*/
+  public E peekTop() throws StackException{
+    if(isEmpty()){
+		  throw new StackException();
+		}
+		for(int i = stack.length - 1; i>= 0; i--){
+			if(stack[i]!= null){
+				return (E)stack[i];
+	    }
+		}
+			return null;
+  }
+	/**
+	* returns the top value in a stack and removes it from the stack.
+	*/
+    public E pop() throws StackException{
+        Object value = new Object();
+        if(isEmpty()){
+            throw new StackException();
+			  }
+				for(int i = stack.length; i >= 0; i--){
+					  if(stack[i] != null){
+							value = stack[i];
+              stack[i] = null;
+              break;
 							 }
 						 }
           
-						 return value;
+						 return (E)value;
            }
-					 
-          public void push (E ob){
+				/**
+				* This method adds an object to a Stack.
+				*/
+		public void push (E ob){
           int index = -1;
 
 						for(int i = 0; i < stack.length; i++){
-							if(stack[i] = null){
+							if(stack[i] == null){
 								index = i;
 							}
 						}
-            if(index = -1){
+            if(index == -1){
 							 Object [] newStack = new Object[stack.length*2];
                for(int i = 0; i<stack.length; i++){
                 newStack[i] = stack[i];
@@ -61,3 +73,4 @@ public class ArrayStack<E> implements Stack<E>
           	}
 }   
   
+}
