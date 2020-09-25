3838ce652a885bdfc83ea61725ac0132f672645e
2190007 <70031113+2190007@users.noreply.github.com>
2020-09-25 12:19:56 -0500

    Update ArrayStackTest.java
    Finished ArrayStackTest.java class.

--- ArrayStackTest.java
+++ ArrayStackTest.java
@@ -14,21 +14,7 @@ public class ArrayStackTest
     ArrayStackTest test = new ArrayStackTest();
     test.test_example();
   }
-
-    /**
-     * This is an example test.  After you have written your own tests,
-     * get rid of it.
-     */
-    @Test
-    public void test_example() {
-        assertTrue("it should be true", 5==5);
-        assertEquals("they should be equal", 5, 5);
-        // when comparing doubles, the last number is a threshold
-        //   of how close the numbers should be to be considered equal
-        assertEquals("they should be with .01 of each other", 5.12, 5.11, .01);
-        if (false)
-            fail("It should have been true");
-    }
+		//This tests the pop method.
     @Test
     public void testPop() throws Exception{
         ArrayStack<Integer> sc = new ArrayStack<Integer>();
@@ -42,4 +28,64 @@ public class ArrayStackTest
             System.out.println("Stack Exception");
         }
     }
+		//This tests the peek top method.
+		@Test
+		public void testPeekTop() throws Exception{
+			ArrayStack<Integer> sc = new ArrayStack<Integer>();
+			sc.push(10);
+			try{
+				assertEquals(new Integer(10), sc.peekTop());
+			}
+			catch(Exception e){
+				System.out.println("Does not work.");
+				System.out.println("Stack Exception");
+			}
+		}
+		//This tests the Push method
+		@Test
+		public void testPush() throws Exception{
+			ArrayStack<Integer> sc = new ArrayStack<Integer>();
+			sc.push(10);
+			try{
+				assertEquals(new Integer(10), sc.peekTop());
+			}
+			catch(Exception e){
+				System.out.println("Does not work.");
+				System.out.println("Stack Exception");
+			}
+		}
+		//This tests the evaluate method.
+		@Test
+		public void testEvaluate(){
+      try{
+      PostfixCalculator c = new PostfixCalculator();
+			String str = "123*+";
+			int num = c.evaluate(str);
+		  assertEquals(new Integer(7), (Integer)num);
+      }
+      catch(Exception e){
+        System.out.println(e);
+      }
+		}
+		//This tests the isEmpty method
+		@Test
+		public void testIsEmpty(){
+			ArrayStack<Integer> sc = new ArrayStack<Integer>();
+			try{
+			assertTrue(sc.isEmpty());
+		}
+		catch(Exception e){
+			System.out.println("Does not work.");
+			System.out.println("Stack Exception");
+		} 
+			sc.push(10);
+			try{
+			assertTrue(!sc.isEmpty());
+		}
+		catch(Exception e){
+			System.out.println("Does not work.");
+			System.out.println("Stack Exception");
+		}
 }
+}
+
